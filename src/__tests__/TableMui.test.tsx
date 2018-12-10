d623c01991579480c0bd0f2f2aa6b1189fcf71f5
Meissner Stephan (DCCS) <smeissner@dccsintra.net>
2018-12-10 15:12:49 +0100

    exported mui table

--- src/__tests__/TableMui.test.tsx
+++ src/__tests__/TableMui.test.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { shallow } from "enzyme";
-import { TableMui } from "../TableMui";
+import { TableMui } from "../index";
 
 it("should render", () => {
   shallow(<TableMui data={[]} desc={false} />);
